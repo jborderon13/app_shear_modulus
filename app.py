ad44ed0bbb05a4d4cc36c576a6a0c851acfad1c5
jborderon13 <124167745+jborderon13@users.noreply.github.com>
2026-09-16 13:58:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -380,7 +380,7 @@ if st.button("Generate curve", type="primary"):
     )
     gamma_log10_pct = np.log10(gamma_pct)
 
-    fig, ax = plt.subplots(figsize=(10, 6))
+    fig, ax = plt.subplots(figsize=(6, 3))
     results = pd.DataFrame(
         {
             "gamma_percent": gamma_pct,
